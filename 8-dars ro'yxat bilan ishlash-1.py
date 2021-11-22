862e5ab90aab6439101e0402187ba33eb0b92de9
muurodov <murodovmuhammadqodir18@gmail.com>
2021-11-22 12:53:46 +0500

    8-dars qo'shildi

---
+++ 8-dars ro'yxat bilan ishlash-1.py
@@ -0,0 +1,22 @@
+10/11/2021
+
+#08-dars:LISTS
+
+#Tartiblash 
+# cars = ['bmw','mercedez benz', 'volvo', 'general motors', 'tesla', 'audi']
+#print(cars)
+
+#narhlar = [12000, 22500, 23456, 9800, 5600, 9934, 32874]
+#arzon = min(narhlar)
+#qimmat = max(narhlar)
+#jammi = sum(narhlar)
+#print("Eng arzon  narh ", arzon, ". Eng qimmati ", qimmat, ". Jami: ", jammi)
+      
+cars = ['bmw', 'mercedes benz', 'volvo', 'gm', 'tesla', 'audi']
+print(cars)
+    
+toys = ('bus','car','bear','dino','snake','lizard')
+print(toys[0])
+print(toys[-1])
+print(toys[2:5])
+
